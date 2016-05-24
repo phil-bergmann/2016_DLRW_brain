7df6913262dfd78fbbae5a8a194c436eb2311db3
Roman C. Podolski <roman.podolski@esrlabs.com>
2016-05-24 14:32:48 +0200

    refactor(data): cleans data pep8

--- data/data.py
+++ data/data.py
@@ -6,22 +6,21 @@
 #
 # Distributed under terms of the MIT license.
 
-
 """
-TODO: Write docstring
+TODO Write docstring
 
 """
 
 from __future__ import print_function
 
-
 import os
-import sys
+# import sys
 import zipfile as z
 
-import scipy.io as sio
+# import scipy.io as sio
 
-def load_way_eeg_gal(shared = True):
+
+def load_way_eeg_gal(shared=True):
     """Loads the WAY-EEG-GAL dataset
 
     If the WAY-EEG-GAL dataset is not present in the ./data/ directory
@@ -30,57 +29,65 @@ def load_way_eeg_gal(shared = True):
     Unzips the dataset and returns the data in a numpy array
 
     :type shared:   boolean
-    :param dataset: move the data to a shared theano tensor, use a numpy array otherwise
+    :param dataset: move the data to a shared theano tensor,
+                    use a numpy array otherwise
 
     :returns: the WAY-EEG-GAL dataset
 
     WARNING!!!!! THE WAY-EEG-GAL dataset is about 15 GB...
     """
 
-
-    origins = [
-            ('P1.zip' , 'https://ndownloader.figshare.com/files/3229301'),
-            ('P2.zip' , 'https://ndownloader.figshare.com/files/3229304'),
-            ('P3.zip' , 'https://ndownloader.figshare.com/files/3229307'),
-            ('P4.zip' , 'https://ndownloader.figshare.com/files/3229310'),
-            ('P5.zip' , 'https://ndownloader.figshare.com/files/3229313')#,
-            # ('P6.zip' , 'https://ndownloader.figshare.com/files/3209486'),
-            # ('P7.zip' , 'https://ndownloader.figshare.com/files/3209501'),
-            # ('P8.zip' , 'https://ndownloader.figshare.com/files/3209504'),
-            # ('P9.zip' , 'https://ndownloader.figshare.com/files/3209495'),
-            # ('P10.zip', 'https://ndownloader.figshare.com/files/3209492'),
-            # ('P11.zip', 'https://ndownloader.figshare.com/files/3209498'),
-            # ('P12.zip', 'https://ndownloader.figshare.com/files/3209489') 
-            ]
+    origins = [('P1.zip', 'https://ndownloader.figshare.com/files/3229301'),
+               ('P2.zip', 'https://ndownloader.figshare.com/files/3229304'),
+               ('P3.zip', 'https://ndownloader.figshare.com/files/3229307'),
+               ('P4.zip', 'https://ndownloader.figshare.com/files/3229310'),
+               ('P5.zip', 'https://ndownloader.figshare.com/files/3229313'),
+               ('P6.zip', 'https://ndownloader.figshare.com/files/3209486'),
+               ('P7.zip', 'https://ndownloader.figshare.com/files/3209501'),
+               ('P8.zip', 'https://ndownloader.figshare.com/files/3209504'),
+               ('P9.zip', 'https://ndownloader.figshare.com/files/3209495'),
+               ('P10.zip', 'https://ndownloader.figshare.com/files/3209492'),
+               ('P11.zip', 'https://ndownloader.figshare.com/files/3209498'),
+               ('P12.zip', 'https://ndownloader.figshare.com/files/3209489')]
 
     for [data_file, url] in origins:
         new_path = os.path.join(os.path.split(__file__)[0], data_file)
 
-        if(not os.path.isfile(new_path)): 
+        if(not os.path.isfile(new_path)):
             from six.moves import urllib
-            import progressbar
+            import progressbar as pb
 
-            pbar = progressbar.ProgressBar().start() 
+            print('Downloading %s from %s saving to %s ...\n' % (
+                data_file, url, new_path
+            ))
+
+            widgets = [pb.Percentage(), ' ',
+                       pb.Timer(), ' ',
+                       pb.Bar(), ' ',
+                       pb.AnimatedMarker(), ' ',
+                       # pb.FileTransferSpeed(), ' ',
+                       pb.ETA()]
+
+            pbar = pb.ProgressBar(widgets=widgets).start()
+
             def rephook(blocks_transfered, block_size, total_size):
                 pbar.maxval = total_size // block_size + 1
                 pbar.update(blocks_transfered)
-                # print('%d bytes / %d bytes, blocks %d, block size %d' % (blocks_transfered * block_size, total_size, blocks_transfered, block_size))
 
-            print('Downloading %s from %s saving to %s ...' % (data_file, url, new_path))
-            urllib.request.urlretrieve(url, new_path, reporthook = rephook)
+            urllib.request.urlretrieve(url, new_path, reporthook=rephook)
 
             pbar.finish()
 
         with z.ZipFile(new_path, 'r') as data_zip:
-            # print(data_zip.namelist())
             for fname in data_zip.namelist():
                 fpath = os.path.join(os.path.split(__file__)[0], fname)
 
                 if(not os.path.isfile(fpath)):
-                    print('Extracting %s to ./data/ ...' % fname)
+                    print('Extracting %s to %s ...' % (
+                        fname, os.path.split(__file__)[0])
+                    )
                     data_zip.extract(fname, os.path.split(__file__)[0])
             # print(sio.loadmat('HS_%s_S1.mat' % data_file[0:2]))
-
 
 if __name__ == "__main__":
     load_way_eeg_gal()
