b77f9e415c361dd219276771faa4623c07264179
phil-bergmann <philipp.bergmann@tum.de>
2016-06-07 23:02:47 +0200

    wip(simple_rnn): Changes to test_RNN.py

--- brain/simple_rnn/test_RNN.py
+++ brain/simple_rnn/test_RNN.py
@@ -7,12 +7,35 @@ import theano.tensor as T
 import climin
 import climin.initialize
 import climin.util
+import glob
+import re
+import zipfile
+import os
+
+import brain.data.globals as st
+from brain.data.util import extract_mat, loadnestedmat
 
 from RNN import RNN
 
-def test_RNN(nh=300, nl=4, n_in=32):
+WS_file_filter_regex = r'WS_P[0-9]*_S[0-9].mat'
+WS_file_filter_regex_P1 = r'WS_P1_S[0-9].mat'
 
-    tmpl = [(n_in, nh), (nh, nh), (nh, nl), nh, nl, nh]  # w is matrix and b a vector
+def test_RNN(nh=300, nl=4, n_in=32):
+    data = []
+    archive_files = glob.glob(st.DATA_PATH + st.P_FILE_REGEX)
+    for archive in archive_files:
+        f_zip = zipfile.ZipFile(archive, 'r')
+        mat_file_list = f_zip.namelist()
+        for f_mat in mat_file_list:
+            if re.search(WS_file_filter_regex_P1, f_mat):
+                if (not os.path.isfile(st.MAT_SUBDIR + f_mat)):
+                    extract_mat(f_zip, f_mat)
+                data.append(loadnestedmat(st.MAT_SUBDIR + f_mat))
+
+    return data
+
+
+    tmpl = [(n_in, nh), (nh, nh), (nh, nl), nh, nl, nh]
     flat, (Wx, Wh, W, bh, b, h0) = climin.util.empty_with_views(tmpl)
     params = [Wx, Wh, W, bh, b, h0]
 
@@ -25,9 +48,3 @@ def test_RNN(nh=300, nl=4, n_in=32):
     def set_pars():
         for p, p_class in zip(params, classifier.params):
             p_class.setValue(p, borrow=True)
-
-            
-
-
-if __name__ == '__main__':
-    test_RNN()
