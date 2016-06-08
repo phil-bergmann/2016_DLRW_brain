b15f6553008da3080bbc2b13be436a1be50f41ba
Dominik <Dominik.irimi@tum.de>
2016-06-08 20:35:34 +0200

    wip (datahandler) util: getTables method added

--- brain/data/util.py
+++ brain/data/util.py
@@ -53,6 +53,42 @@ def extract_mat(zf, filename, relative_path=''):
     except KeyError:
         print 'ERROR: Did not find %s in zip file' % filename
 
+
+class Regexhandler(object):
+
+    def __init__(self, reg):
+        self.regex = reg
+
+    def get_series(self):
+        try:
+            start_pattern = re.compile('_S')
+            series_start = start_pattern.search(self.regex)
+            if series_start is not None:
+                series_start = series_start.regs[0][1]
+            end_pattern = re.compile('_|\.mat')
+            series_end = end_pattern.search(self.regex, series_start)
+            if series_end is not None:
+                series_end = series_end.regs[0][0]
+            return self.regex[series_start : series_end]
+        except:
+            raise Exception('Failed to extract series')
+        raise Exception('No series specified')
+
+    def get_participant(self):
+        try:
+            start_pattern = re.compile('_P')
+            participant_start = start_pattern.search(self.regex)
+            if participant_start is not None:
+                participant_start = participant_start.regs[0][1]
+            end_pattern = re.compile('_|\.mat')
+            participant_end = end_pattern.search(self.regex, participant_start)
+            if participant_end is not None:
+                participant_end = participant_end.regs[0][0]
+            return self.regex[participant_start : participant_end]
+        except:
+            raise Exception('Failed to extract participant')
+        raise Exception('No participant specified')
+
 def getTables(regex):
     '''
     A function that returns all Tables matching to a particular regular expression
@@ -60,14 +96,33 @@ def getTables(regex):
     
     e.g. r'WS_P1_S[0-9].mat' should return all windowed session tables as a list from Person 1
     '''
+
+    reghandler = Regexhandler(regex)
+    series_regex = reghandler.get_series()
+    participant_regex = reghandler.get_participant()
+
     data = []
-    archive_files = glob.glob(st.DATA_PATH + st.P_FILE_REGEX)
+    archive_files = glob.glob('../'+st.DATA_PATH + st.P_FILE_REGEX)
     for archive in archive_files:
         f_zip = zipfile.ZipFile(archive, 'r')
         mat_file_list = f_zip.namelist()
         for f_mat in mat_file_list:
             if re.search(regex, f_mat):
-                if (not os.path.isfile(st.MAT_SUBDIR + f_mat)):
-                    extract_mat(f_zip, f_mat)
-                data.append(loadnestedmat(st.MAT_SUBDIR + f_mat))
+                if not os.path.isfile('../'+st.DATA_PATH + st.MAT_SUBDIR + f_mat):
+                    mat = extract_mat(f_zip, f_mat)
+
+                    ws = mat.get('ws')
+                    participant = ws.get('participant')
+                    series = ws.get('series')
+
+                    series_pattern = re.compile(series_regex)
+                    series_target = series_pattern.search(str(series))
+
+                    series_pattern = re.compile(participant_regex)
+                    participant_target = series_pattern.search(str(participant))
+
+                    if (series_target is not None) and (participant_target is not None):
+                        ws = mat.get('ws')
+                        for win in enumerate(ws.get('win')):
+                            data.append(win[1])
     return data
