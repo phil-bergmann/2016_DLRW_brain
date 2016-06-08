4d6698841b90375e2aebed904969b7a1342b575d
Dominik <Dominik.irimi@tum.de>
2016-06-08 21:17:37 +0200

    wip (datahandler) bugfix util getTables

--- brain/data/test_getTables.py
+++ brain/data/test_getTables.py
@@ -1,7 +1,7 @@
 from util import getTables
 
 if __name__ == "__main__":
-    table = getTables(r'WS_P[0-9]_S1.mat')
-    print len(table)
     table = getTables(r'WS_P1_S[0-9].mat')
+    print len(table)
+    table = getTables(r'WS_P[0-9]_S1.mat')
     print len(table)
