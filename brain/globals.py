3094c67b1382394555560a8120173b02f744e69a
phil-bergmann <philipp.bergmann@tum.de>
2016-07-09 16:11:26 +0200

    chore(globals): Add a target sequence global var

--- brain/globals.py
+++ brain/globals.py
@@ -7,8 +7,15 @@ N_P_FILES = 12
 
 N_EEG_SENSORS = 32
 N_EMG_SENSORS = 5
+
+# defines which events to select and their sequence (0: deactivated, 1: First, 2: Second, ...)
+SEQ_EMG_TARGETS = [1, 0, 0, 0, 2]
 N_EMG_TARGETS = 2
+
+# defines which events to select and their sequence (0: deactivated, 1: First, 2: Second, ...)
+SEQ_EEG_TARGETS = [1, 2, 0, 0, 0]
 N_EEG_TARGETS = 2
+
 
 N_EEG_TIMESTEPS = 6200
 N_EMG_TIMESTEPS = 50000
