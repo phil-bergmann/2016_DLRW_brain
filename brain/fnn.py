cb22ae54b7116e992d65d9b867fdee5678395fab
ManuelNickel <manuel.nickel@tum.de>
2016-07-24 17:56:26 +0200

    <doc>(final_report.tex): Section about tsne (first draft)

--- brain/fnn.py
+++ brain/fnn.py
@@ -19,6 +19,7 @@ from data import normalize
 import climin, climin.util, climin.initialize
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
+from scipy import signal
 from sys import stdout
 
 class HiddenLayer(object):
@@ -99,222 +100,239 @@ class MLP(object):
 
 
 
-def test_mlp(learning_rate=0.01, L1_reg=0.00, L2_reg=0.0001, n_epochs=100,
-             batch_size=100, n_hidden=300, optimizer='GradientDescent', activation=T.tanh):
+def test_mlp(learning_rate=0.01, L1_reg=0.00, L2_reg=0.0001, n_epochs=20,
+             batch_size=100, n_hidden=300, optimizer='GradientDescent', activation=T.tanh, a=(1, -0.98), b=(1, -1)):
 
     #---- Configure ----
-    participant = 7
-    series = 8
+    participant = 1
+    series = 1
+    no_series = 1
     datatype = 'eeg'
     trials_from = 1
     trials_to = 'end'
     normalize_data = False
-    normalize_per_trial = True
+    normalize_per_trial = False
     #-------------------
 
-    error_lists = [None, None]
-    for i in [0, 1]:
+    """error_lists = [None, None]
+    for i in [ 1]:
         if(i == 0):
             normalize_data = True
             normalize_per_trial = False
         else:
             normalize_data = False
-            normalize_per_trial = True
+            normalize_per_trial = True"""
 
-        # Get data
-        ws = get_ws(participant=participant, series=series)
+    # Get data
+    ws = get_ws(participant=participant, series=series)
+    windows = ws.get('win')
+    (data, trials, led) = get_data(windows, datatype=datatype, trials_from=trials_from, trials_to=trials_to,
+                                             normalize_per_trial=normalize_per_trial)
+    for i in range(no_series-1):
+        ws = get_ws(participant=participant, series=series+i+1)
         windows = ws.get('win')
-        (data, trials, led) = get_data(windows, datatype=datatype, trials_from=trials_from, trials_to=trials_to,
-                                       normalize_per_trial=normalize_per_trial)
-        if normalize_data:
-            data[...] = normalize(data)
-        (temp, undo_shuffle) = shuffle(np.c_[data, trials - 1])
-        n = data.shape[0]
-        n_train = 4 * n // 9
-        n_valid = 2 * n // 9
-        n_test = n - n_train - n_valid
-
-        train_set_x, train_set_y = (temp[:n_train, :data.shape[1]], temp[:n_train, data.shape[1]:])
-        valid_set_x, valid_set_y = (temp[n_train:n_train+n_valid, :data.shape[1]], temp[n_train:n_train+n_valid, data.shape[1]:])
-        test_set_x, test_set_y = (temp[n_train+n_valid:, :data.shape[1]], temp[n_train+n_valid:, data.shape[1]:])
-
-        # Reshaping data from (n,1) to (n,)
-        train_set_y = train_set_y.reshape(train_set_y.shape[0],)
-        valid_set_y = valid_set_y.reshape(valid_set_y.shape[0], )
-        test_set_y = test_set_y.reshape(test_set_y.shape[0], )
+        (data_temp, trials_temp, led) = get_data(windows, datatype=datatype, trials_from=trials_from, trials_to=trials_to,
+                                   normalize_per_trial=normalize_per_trial)
+        data = np.vstack((data, data_temp))
+        trials = np.concatenate((trials, trials_temp + trials[-1]))
 
 
-
-        n_train_batches = train_set_x.shape[0] // batch_size
-        print('Building the Model...')
-
-        x = T.matrix('x')
-        y = T.ivector('y')
-
-        rng = np.random.RandomState(1234)
-
-        n_in = data.shape[1]
-        n_out = 34
-        dims = [(n_in, n_hidden), n_hidden, (n_hidden, n_out), n_out]
-        flat, (hidden_W, hidden_b, logreg_W, logreg_b) = climin.util.empty_with_views(dims)
-        climin.initialize.randomize_normal(flat, loc=0, scale=0.1)
-        #hidden_W[...] = np.asarray(rng.uniform(low=-4*np.sqrt(6. / (n_in + n_hidden)), high=4*np.sqrt(6. / (n_in + n_hidden)), size=(n_in, n_hidden)))
-        parameters = [theano.shared(value = hidden_W, name = 'W', borrow = True),
-                      theano.shared(value=hidden_b, name='b', borrow=True),
-                      theano.shared(value = logreg_W, name = 'W', borrow = True),
-                      theano.shared(value=logreg_b, name='b', borrow=True)]
-
-        classifier = MLP(
-            rng=rng,
-            input=x,
-            n_in=n_in,
-            n_hidden=n_hidden,
-            n_out=n_out,
-            activation=activation,
-            parameters=parameters
-        )
+    # Filtering
+    #a = (1, -0.98)
+    #b = (1, -1)
+    data = signal.filtfilt(b, a, data)
 
 
-        cost = classifier.negative_log_likelihood(y) + L1_reg * classifier.L1 + L2_reg * classifier.L2_sqr
-        gparams = [T.grad(cost, param) for param in classifier.params]
+    if normalize_data:
+        data[...] = normalize(data)
+    (temp, undo_shuffle) = shuffle(np.c_[data, trials - 1])
+    n = data.shape[0]
+    n_train = 4 * n // 9
+    n_valid = 2 * n // 9
+    n_test = n - n_train - n_valid
 
+    train_set_x, train_set_y = (temp[:n_train, :data.shape[1]], temp[:n_train, data.shape[1]:])
+    valid_set_x, valid_set_y = (temp[n_train:n_train+n_valid, :data.shape[1]], temp[n_train:n_train+n_valid, data.shape[1]:])
+    test_set_x, test_set_y = (temp[n_train+n_valid:, :data.shape[1]], temp[n_train+n_valid:, data.shape[1]:])
 
-        """ Theano functions """
-        grad_W = theano.function([x, y], gparams, allow_input_downcast=True)
-
-
-        print('Setting up Climin...')
-        """ Setting up Climin """
-
-        def d_loss(parameters, inputs, targets):
-            g_hl_W, g_hl_b, g_lr_W, g_lr_b = grad_W(inputs, targets)
-
-            return np.concatenate([g_hl_W.flatten(), g_hl_b, g_lr_W.flatten(), g_lr_b])
+    # Reshaping data from (n,1) to (n,)
+    train_set_y = train_set_y.reshape(train_set_y.shape[0],)
+    valid_set_y = valid_set_y.reshape(valid_set_y.shape[0], )
+    test_set_y = test_set_y.reshape(test_set_y.shape[0], )
 
 
 
-        minibatch = True
-        if not minibatch:
-            args = itertools.repeat(([train_set_x, train_set_y], {}))
-        else:
-            args = ((i, {}) for i in climin.util.iter_minibatches([train_set_x, train_set_y], batch_size, [0, 0]))
+    n_train_batches = train_set_x.shape[0] // batch_size
+    #print('Building the Model...')
 
-        if optimizer=='GradientDescent':
-            print('Running GradientDescent')
-            opt = climin.GradientDescent(flat, d_loss, step_rate=0.01, momentum=0.95, args=args)
-        elif optimizer=='RmsProp':
-            print('Running RmsProp')
-            opt = climin.rmsprop.RmsProp(flat, d_loss, step_rate=0.01, args=args)
-        #elif optimizer == 'NonlinearConjugateGradient':
-        #    opt = climin.cg.NonlinearConjugateGradient(d_loss, loss, d_loss, min_grad=1e-06, args=args)
-        elif optimizer == 'Adadelta':
-            print('Running Adadelta')
-            opt = climin.adadelta.Adadelta(flat, d_loss, step_rate=0.01, decay=0.9, momentum=0, offset=0.001, args=args)
-        elif optimizer == 'Adam':
-            print('Running Adam')
-            opt = climin.adam.Adam(flat, d_loss, step_rate=0.001, decay=0.3, decay_mom1=0.1,
-                                        decay_mom2=0.001, momentum=0, offset=1e-08, args=args)
-        elif optimizer == 'Rprop':
-            print('Running Rprop')
-            opt = climin.rprop.Rprop(flat, d_loss, step_shrink=0.5, step_grow=1.2, min_step=1e-06, max_step=1,
-                                          changes_max=0.1, args=args)
-        else:
-            print('Optimizer not available!')
-            opt = None
+    x = T.matrix('x')
+    y = T.ivector('y')
 
-        zero_one_loss = theano.function(
-            inputs=[x, y],
-            outputs=classifier.logRegressionLayer.errors(y),
-            allow_input_downcast=True
-        )
+    rng = np.random.RandomState(1234)
 
-        print('Running Optimization...\n')
+    n_in = data.shape[1]
+    n_out = np.unique(trials).shape[0]
+    dims = [(n_in, n_hidden), n_hidden, (n_hidden, n_out), n_out]
+    flat, (hidden_W, hidden_b, logreg_W, logreg_b) = climin.util.empty_with_views(dims)
+    climin.initialize.randomize_normal(flat, loc=0, scale=0.1)
+    #hidden_W[...] = np.asarray(rng.uniform(low=-4*np.sqrt(6. / (n_in + n_hidden)), high=4*np.sqrt(6. / (n_in + n_hidden)), size=(n_in, n_hidden)))
+    parameters = [theano.shared(value = hidden_W, name = 'W', borrow = True),
+                  theano.shared(value=hidden_b, name='b', borrow=True),
+                  theano.shared(value = logreg_W, name = 'W', borrow = True),
+                  theano.shared(value=logreg_b, name='b', borrow=True)]
 
-        patience = 10000
-        patience_increase = 4
-        improvement_threshold = 0.995
-        validation_frequency = min(n_train_batches, patience // 2)
-
-        best_validation_loss = np.inf
-        best_iter = 0
-        start_time = timeit.default_timer()
-
-        epoch = 0
-        done_looping = False
-
-        train_error_list = []
-        valid_error_list = []
-        test_error_list = []
-
-        #model = Model(classifier.params)
-
-        train_score = zero_one_loss(train_set_x, train_set_y) * 100
-        this_validation_loss = zero_one_loss(valid_set_x, valid_set_y) * 100
-        test_score = zero_one_loss(test_set_x, test_set_y) * 100
-
-        train_error_list.append(train_score)
-        valid_error_list.append(this_validation_loss)
-        test_error_list.append(test_score)
+    classifier = MLP(
+        rng=rng,
+        input=x,
+        n_in=n_in,
+        n_hidden=n_hidden,
+        n_out=n_out,
+        activation=activation,
+        parameters=parameters
+    )
 
 
-        for info in opt:
-            iter = info['n_iter']
+    cost = classifier.negative_log_likelihood(y) + L1_reg * classifier.L1 + L2_reg * classifier.L2_sqr
+    gparams = [T.grad(cost, param) for param in classifier.params]
 
-            if (iter % 1)==0:
-                stdout.write("\r%f%% of Epoch %d" % (float(iter * 100)/n_train_batches - epoch * 100, epoch))
-                stdout.flush()
 
-            if (iter + 1) % validation_frequency == 1:
-                epoch += 1
+    """ Theano functions """
+    grad_W = theano.function([x, y], gparams, allow_input_downcast=True)
 
-                train_score = zero_one_loss(train_set_x, train_set_y) * 100
-                this_validation_loss = zero_one_loss(valid_set_x, valid_set_y) * 100
-                test_score = zero_one_loss(test_set_x, test_set_y) * 100
 
-                train_error_list.append(train_score)
-                valid_error_list.append(this_validation_loss)
-                test_error_list.append(test_score)
+    #print('Setting up Climin...')
+    """ Setting up Climin """
+
+    def d_loss(parameters, inputs, targets):
+        g_hl_W, g_hl_b, g_lr_W, g_lr_b = grad_W(inputs, targets)
+
+        return np.concatenate([g_hl_W.flatten(), g_hl_b, g_lr_W.flatten(), g_lr_b])
 
 
 
-                print('\nEpoch %i, Validation Error:\t %f%%' % (epoch, this_validation_loss))
+    minibatch = True
+    if not minibatch:
+        args = itertools.repeat(([train_set_x, train_set_y], {}))
+    else:
+        args = ((i, {}) for i in climin.util.iter_minibatches([train_set_x, train_set_y], batch_size, [0, 0]))
 
-                if this_validation_loss < best_validation_loss:
-                    if (this_validation_loss < best_validation_loss * improvement_threshold):
-                        patience = max(patience, iter * patience_increase)
+    if optimizer=='GradientDescent':
+        #print('Running GradientDescent')
+        opt = climin.GradientDescent(flat, d_loss, step_rate=0.01, momentum=0.95, args=args)
+    elif optimizer=='RmsProp':
+        print('Running RmsProp')
+        opt = climin.rmsprop.RmsProp(flat, d_loss, step_rate=0.01, args=args)
+    #elif optimizer == 'NonlinearConjugateGradient':
+    #    opt = climin.cg.NonlinearConjugateGradient(d_loss, loss, d_loss, min_grad=1e-06, args=args)
+    elif optimizer == 'Adadelta':
+        print('Running Adadelta')
+        opt = climin.adadelta.Adadelta(flat, d_loss, step_rate=0.01, decay=0.9, momentum=0, offset=0.001, args=args)
+    elif optimizer == 'Adam':
+        print('Running Adam')
+        opt = climin.adam.Adam(flat, d_loss, step_rate=0.001, decay=0.3, decay_mom1=0.1,
+                                    decay_mom2=0.001, momentum=0, offset=1e-08, args=args)
+    elif optimizer == 'Rprop':
+        print('Running Rprop')
+        opt = climin.rprop.Rprop(flat, d_loss, step_shrink=0.5, step_grow=1.2, min_step=1e-06, max_step=1,
+                                      changes_max=0.1, args=args)
+    else:
+        print('Optimizer not available!')
+        opt = None
 
-                    best_validation_loss = this_validation_loss
-                    best_test_score = test_score
-                    best_iter = iter
+    zero_one_loss = theano.function(
+        inputs=[x, y],
+        outputs=classifier.logRegressionLayer.errors(y),
+        allow_input_downcast=True
+    )
 
-                    print(('Epoch %i, Test Error:\t %f%% \t NEW MODEL') % (epoch, test_score))
-                    #with open('model.pkl', 'wb') as f:
-                        #print('Dump Model')
-                    #    pickle.dump(model, f)
+    #print('Running Optimization...\n')
+    #print('Classifying %d classes' % n_out)
 
-                if (epoch >= n_epochs) or done_looping:
-                    break
+    patience = 10000
+    patience_increase = 2
+    improvement_threshold = 0.995
+    validation_frequency = n_train_batches#min(n_train_batches, patience // 2)
 
-                print ('')
+    best_validation_loss = np.inf
+    best_iter = 0
+    start_time = timeit.default_timer()
 
-            if patience <= iter:
-                done_looping = True
+    epoch = 0
+    done_looping = False
+
+    train_error_list = []
+    valid_error_list = []
+    test_error_list = []
+
+    #model = Model(classifier.params)
+
+    train_score = zero_one_loss(train_set_x, train_set_y) * 100
+    this_validation_loss = zero_one_loss(valid_set_x, valid_set_y) * 100
+    test_score = zero_one_loss(test_set_x, test_set_y) * 100
+
+    train_error_list.append(train_score)
+    valid_error_list.append(this_validation_loss)
+    test_error_list.append(test_score)
+
+
+    for info in opt:
+        iter = info['n_iter']
+
+        """if (iter % 1)==0:
+            stdout.write("\r%f%% of Epoch %d" % (float(iter * 100)/n_train_batches - epoch * 100, epoch))
+            stdout.flush()"""
+
+        if (iter + 1) % validation_frequency == 1:
+            epoch += 1
+
+            train_score = zero_one_loss(train_set_x, train_set_y) * 100
+            this_validation_loss = zero_one_loss(valid_set_x, valid_set_y) * 100
+            test_score = zero_one_loss(test_set_x, test_set_y) * 100
+
+            train_error_list.append(train_score)
+            valid_error_list.append(this_validation_loss)
+            test_error_list.append(test_score)
+
+
+
+            #print('\nEpoch %i, Validation Error:\t %f%%' % (epoch, this_validation_loss))
+
+            if this_validation_loss < best_validation_loss:
+                if (this_validation_loss < best_validation_loss * improvement_threshold):
+                    patience = max(patience, iter * patience_increase)
+
+                best_validation_loss = this_validation_loss
+                best_test_score = test_score
+                best_iter = iter
+
+                #print(('Epoch %i, Test Error:\t %f%% \t NEW MODEL') % (epoch, test_score))
+                #with open('model.pkl', 'wb') as f:
+                    #print('Dump Model')
+                #    pickle.dump(model, f)
+
+            if (epoch >= n_epochs) or done_looping:
                 break
 
+            #print ('')
 
-        #scores = Scores(train_error_list, valid_error_list, test_error_list, [best_validation_loss, test_score])
-        #with open('scores.pkl', 'wb') as f:
-        #    pickle.dump(scores, f)
+        if patience <= iter:
+            done_looping = True
+            break
 
-        end_time = timeit.default_timer()
-        print(('Optimization complete. Best validation score of %f %% '
-               'obtained at iteration %i, with test performance %f %%') %
-              (best_validation_loss, best_iter + 1, best_test_score))
-        print(('The code for file ' + os.path.split(__file__)[1] + ' ran for %.2fm' % ((end_time - start_time) / 60.)), file=sys.stderr)
 
-        error_lists[i] = (train_error_list, valid_error_list, test_error_list)
-    #return (train_error_list, valid_error_list, test_error_list), (best_validation_loss, best_test_score)
-    return error_lists, (best_validation_loss, best_test_score)
+    #scores = Scores(train_error_list, valid_error_list, test_error_list, [best_validation_loss, test_score])
+    #with open('scores.pkl', 'wb') as f:
+    #    pickle.dump(scores, f)
+
+    end_time = timeit.default_timer()
+    print(('Optimization complete. Best validation score of %f %% '
+           'obtained at iteration %i, with test performance %f %%') %
+          (best_validation_loss, best_iter + 1, best_test_score))
+    print(('The code for file ' + os.path.split(__file__)[1] + ' ran for %.2fm' % ((end_time - start_time) / 60.)), file=sys.stderr)
+
+        #error_lists[i] = (train_error_list, valid_error_list, test_error_list)
+    return (train_error_list, valid_error_list, test_error_list), (best_validation_loss, best_test_score)
+    #return error_lists, (best_validation_loss, best_test_score)
 
 
 
@@ -377,11 +395,44 @@ def plot_error_curves2(error_lists1, error_lists2, best_scores, args=('Sigmoid',
     plt.show()
 
 
+def optimize_filtfilt(activation, optimizer, step_size=0.01, n=5):
+    a = (1, -0.98)
+    b = (1, -1)
+    flat_t = np.concatenate((a, b))
+
+    print("Running NN with: a=(%f, %f), b=(%f, %f)" % (flat_t[0], flat_t[1], flat_t[2], flat_t[3]))
+    lists, best_scores = test_mlp(activation=activation, optimizer=optimizer, a=a, b=b)
+    y_t = 1 - best_scores[1]/100
+    y_best = y_t
+    flat_best = flat_t
+    n_best = 0
+
+    flat = flat_t + step_size * 10
+
+    for i in range(n):
+        print("\n\nRunning NN with: a=(%f, %f), b=(%f, %f)" % (flat[0], flat[1], flat[2], flat[3]))
+        lists, best_scores = test_mlp(activation=activation, optimizer=optimizer, a=(flat[0], flat[1]), b=(flat[2], flat[3]))
+        y = 1 - best_scores[1] / 100
+
+        if(y < y_best):
+            y_best = y
+            flat_best = flat
+            n_best = i
+
+        flat = flat - step_size * (y - y_t) / (flat - flat_t)
+        y_t = y
+        flat_t = flat
+
+    print('Found best model in iteraton %d with y=%f and' % (n_best, y_best))
+    print("a=(%f, %f), b=(%f, %f)" % (flat_best[0], flat_best[1], flat_best[2], flat_best[3]))
+
+
 if __name__ == '__main__':
     #activation = T.nnet.sigmoid
     activation = T.tanh
     #activation = T.nnet.relu
     optimizer = 'GradientDescent'
-    lists, best_scores = test_mlp(activation=activation, optimizer=optimizer)
+    optimize_filtfilt(activation=activation, optimizer=optimizer)
+    #lists, best_scores = test_mlp(activation=activation, optimizer=optimizer)
     #plot_error_curves(lists, best_scores, args=('tanh', optimizer))
-    plot_error_curves2(lists[0], lists[1], best_scores, args=('tanh', optimizer))
+    #plot_error_curves2(lists[0], lists[1], best_scores, args=('tanh', optimizer))
